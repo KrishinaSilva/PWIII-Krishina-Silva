1d9badd15858cc7b045051c9c9e9e106e4db67b8
Krishina Silva <krishinasilva03@hotmail.com>
2025-04-28 22:11:50 -0300

    Atividade realizada com sucesso e sem erros

---
+++ aula-api/pages/api/create.js
@@ -0,0 +1,47 @@
+//pages/api/create.js
+import { createConnection } from "mysql2/promise";
+
+//Função para conectar no MySQL
+async function connectToDatabase() {
+    return createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'teste-api',
+    });
+}
+
+export default async function handler(req, res) {
+    if (req.method !== 'POST') {
+        return res.status(405).json({ error: 'Método não permitido'});
+    }
+
+    const userdata = req.body;
+    console.log(userdata)
+
+    const { name, email } = userdata;
+
+    if (!name || !email) {
+        return res.status(400).json({ error: 'name e email são obrigatórios no request body.' });
+    }
+    try {
+        // Conecta no Banco
+        const connection = await connectToDatabase();
+
+        //Execução da query para transacionar dados da tabela "users"
+        const [result] = await connection.execute('INSERT INTO users (name, email) VALUES (?, ?)', [
+            name,
+            email,
+        ]);
+        // Checa se o usuário existe
+
+        //Encerra a conexão
+        await connection.end();
+
+        //Resposta com os dados do usuário
+        res.status(201).json({ id: result.insertId, message: 'Usuário criado com sucesso!' });
+    } catch (error) {
+      console.error('Erro de conexão com o banco:', error);
+      res.status(500).json({ error: 'Erro Interno de Servidor' });  
+    }
+}
